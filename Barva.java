d96c537086885c23ced1393d261f60aa190f8181
Buk01 <mudra.jakub@gmail.com>
2020-05-18 15:32:34 +0000

    tak slava

---
+++ Barva.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ import java.util.Random;
+/**
+ *
+ * @author Ucebna
+ */
+public enum Barva {
+    ZELENA, MODRA, CERVENA, ZLUTA, FIALOVA;
+
+    public static Barva nahodnaBarva(){
+      return values()[new Random().nextInt(values().length)];
+    }
+}
